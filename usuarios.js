5796f8157bc210556b8cb93194cdecddf0075f93
brunoscheifer <brunoscheifer11@gmail.com>
2024-08-07 17:01:13 -0300

    terminei

--- usuarios.js
+++ usuarios.js
@@ -1,4 +1,4 @@
 module.exports = [
-    {id: 1, nome: 'Bruno', email: 'bruno@gmail.com', telefones: '99999999'},
-    {id: 2, nome: 'pessoa', email: 'pessoa@gmail.com', telefones: '88888888'}
+    {id: 1, nome: 'Bruno', email: 'bruno@gmail.com', telefones: ['99999999', '66666666']},
+    {id: 2, nome: 'pessoa', email: 'pessoa@gmail.com', telefones: ['88888888', '77777777']}
 ]
